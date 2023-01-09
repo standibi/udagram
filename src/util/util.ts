63e0b8a2bd573274033075a891449b56ee67c970
Stanislas DIBI <stanislasdibi@consultech.ci>
2023-01-09 17:50:12 +0000

    fix: Jimp.read sometimes fails

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import Jimp = require("jimp");
+import axios from "axios";
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -11,7 +12,8 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const { data } = await axios.get(inputURL, { responseType: "arraybuffer" });
+      const photo = await Jimp.read(data);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
@@ -38,7 +40,7 @@ export async function deleteLocalFiles(files: Array<string>) {
   }
 }
 
-export function isValidUrl(url: string) {
+export function isValidUrl(url: string): boolean {
   try {
     return new Boolean(new URL(url)).valueOf();
   }
